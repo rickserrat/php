2e6c57ffca63ad22c6de9966a0f1efafa5c3e5a1
HENRIQUE SERRAT GUIMARAES <henriqueserrat@gmail.com>
2020-04-03 16:43:24 -0300

    Create radomReason.php

---
+++ radomReason.php
@@ -0,0 +1,34 @@
+$listReasons = [
+            'Reason 1',
+            'Reason 2',
+            'Reason 3',
+            'Reason 4',
+        ];
+
+/**
+ * Randomize list with refund reasons.
+ *
+ * @param $list
+ * @return array
+ */
+function randomReason($list) {
+      if (!is_array($list)) {
+          return $list;
+      }
+      $keys = array_keys($list);
+      shuffle($keys);
+      $random = array();
+
+      foreach ($keys as $key) {
+          $random[$list[$key]] = $list[$key];
+      }
+
+      $option = [''=> 'Selecione o motivo da devolução'];
+
+      return $option + $random;
+  }
+
+  $reasons = randomReason($listReasons);
+
+  return $reasons
+}
