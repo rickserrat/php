ccf05e03bf68ba6a97a4dea2e59524e61eb53388
HENRIQUE SERRAT GUIMARAES <henriqueserrat@gmail.com>
2020-06-12 15:40:41 -0300

    Update ArrayLastKey.php
    Adicionando função para pegar última key do array com PHP 7 for maior ou igual ao PHP 7.3.0

--- ArrayLastKey.php
+++ ArrayLastKey.php
@@ -5,7 +5,17 @@ $arr = [44,32,34,87];
 end($arr);
 
 $a = key($arr);
-//$a = int(3)
+//$a -> int(3)
 
 echo $arr[$a];
+//87
+
+//----
+
+//Pega a última key do array. (PHP 7 >= 7.3.0)
+$arr2 = [44,32,34,87];
+$b = array_key_last ($arr2);
+//$b -> int(3) 
+
+echo $arr2[$b];
 //87
